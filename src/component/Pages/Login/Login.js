50fc8ebda4a43d0c5a17a24d1d0ce1dc7508c6ca
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-25 01:30:33 +0600

    login form

--- src/component/Pages/Login/Login.js
+++ src/component/Pages/Login/Login.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../Login/login.svg'
 
 const Login = () => {
     return (
         <div>
-            <h2>Login</h2>
+            <div className="hero my-20 w-full">
+        <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lag:flex-row ">
+          <div className="text-center lg:text-left">
+            <img className='w-3/4' src={img} alt="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-5 ">
+            <h1 className="text-5xl font-bold text-center">Login now!</h1>
+            <form  className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                <label className="label">
+                  <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                </label>
+
+              </div>
+              <div className="form-control mt-6">
+                <input type="submit" className="btn btn-primary" value="login" />
+              </div>
+              <label className="label">
+                <p className='text-center mt-5'>New To Genius Car - <Link className='text-orange-600 font-bold' to='/register'>Sign Up</Link> </p>
+              </label>
+            </form>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
--- src/component/Pages/Shared/Footer/Footer.js
+++ src/component/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
 const Footer = () => {
+    
     return (
-        <div>
-            <h2>Footer</h2>
-        </div>
+        <footer 
+        // style={{
+        //     background: `url(${})`,
+        //     backgroundSize: 'cover'
+        // }}
+        >
+            <div className="footer p-10 m-10">
+            <div>
+                <span className="footer-title">Services</span>
+                <a href='/' className="link link-hover">Branding</a>
+                <a href='/' className="link link-hover">Design</a>
+                <a href='/' className="link link-hover">Marketing</a>
+                <a href='/' className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a href='/' className="link link-hover">About us</a>
+                <a href='/' className="link link-hover">Contact</a>
+                <a href='/' className="link link-hover">Jobs</a>
+                <a href='/' className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a href='/' className="link link-hover">Terms of use</a>
+                <a href='/' className="link link-hover">Privacy policy</a>
+                <a href='/' className="link link-hover">Cookie policy</a>
+            </div>
+            </div>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2022</p>
+            </div>
+        </footer>
     );
 };
 
