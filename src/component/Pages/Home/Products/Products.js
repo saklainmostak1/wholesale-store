e7ed5cfb96edc8ed87ca490f8f99d468dfd7a426
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-26 03:21:35 +0600

    dashboard

--- src/component/Pages/Home/Products/Products.js
+++ src/component/Pages/Home/Products/Products.js
@@ -27,7 +27,7 @@ const Products = () => {
     return (
         <div>
             
-
+      <h2 className='text-3xl font-bold text-center'>All Products</h2>
         <div className=' m-5 grid mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
            {
                products.map(product => <ProductsCard
